9f5e93727976c62d6a49c1eb2a22e2aa84ae7a30
Gaëtan Schwartz <>
2025-09-23 12:22:45 +0200

    f

--- serde-kdl-macro/src/specs/mod.rs
+++ serde-kdl-macro/src/specs/mod.rs
@@ -16,62 +16,62 @@ macro_rules! assert_eq_tk {
     }};
 }
 
-#[cfg(test)]
-mod section_3_1_test;
+// #[cfg(test)]
+// mod section_3_1_test;
 
-#[cfg(test)]
-mod section_3_2_test;
+// #[cfg(test)]
+// mod section_3_2_test;
 
-#[cfg(test)]
-mod section_3_3_test;
+// #[cfg(test)]
+// mod section_3_3_test;
 
-#[cfg(test)]
-mod section_3_4_test;
+// #[cfg(test)]
+// mod section_3_4_test;
 
-#[cfg(test)]
-mod section_3_5_test;
+// #[cfg(test)]
+// mod section_3_5_test;
 
-#[cfg(test)]
-mod section_3_6_test;
+// #[cfg(test)]
+// mod section_3_6_test;
 
-#[cfg(test)]
-mod section_3_7_test;
+// #[cfg(test)]
+// mod section_3_7_test;
 
-#[cfg(test)]
-mod section_3_8_test;
+// #[cfg(test)]
+// mod section_3_8_test;
 
-#[cfg(test)]
-mod section_3_9_test;
+// #[cfg(test)]
+// mod section_3_9_test;
 
-#[cfg(test)]
-mod section_3_10_test;
+// #[cfg(test)]
+// mod section_3_10_test;
 
-#[cfg(test)]
-mod section_3_11_test;
+// #[cfg(test)]
+// mod section_3_11_test;
 
-#[cfg(test)]
-mod section_3_12_test;
+// #[cfg(test)]
+// mod section_3_12_test;
 
-#[cfg(test)]
-mod section_3_13_test;
+// #[cfg(test)]
+// mod section_3_13_test;
 
-#[cfg(test)]
-mod section_3_14_test;
+// #[cfg(test)]
+// mod section_3_14_test;
 
-#[cfg(test)]
-mod section_3_15_test;
+// #[cfg(test)]
+// mod section_3_15_test;
 
-#[cfg(test)]
-mod section_3_16_test;
+// #[cfg(test)]
+// mod section_3_16_test;
 
-#[cfg(test)]
-mod section_3_17_test;
+// #[cfg(test)]
+// mod section_3_17_test;
 
-#[cfg(test)]
-mod section_3_18_test;
+// #[cfg(test)]
+// mod section_3_18_test;
 
-#[cfg(test)]
-mod section_3_19_test;
+// #[cfg(test)]
+// mod section_3_19_test;
 
-#[cfg(test)]
-mod section_4_1_test;
+// #[cfg(test)]
+// mod section_4_1_test;
